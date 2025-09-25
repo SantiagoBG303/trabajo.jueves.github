04b146cfb47fc67cccfe03f37dea9af7911b7e97
santiago <santiago.bermudezg@uniagustiniana.edu.co>
2025-09-25 21:02:33 +0000

    actualizaacion

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,3 +1,15 @@
-function Pokemon(){
-    document.getElementById("root").innerHTML = "pokemon"
+async function Pokemon(id){
+    
+
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`); 
+    const data = await res.json();
+
+    var root = document.getElementById("root");
+    root.innerHTML = data.forms[0].name
 }
+
+Pokemon(5)
+Pokemon(6)
+Pokemon(7)
+Pokemon(25)
